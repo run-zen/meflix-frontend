cd209a3b3aff3557f42725fe5aaa8bdce8e51eaf
runzen <55329832+run-zen@users.noreply.github.com>
2021-05-21 01:53:51 +0530

    pagination loading starts at the 4th movie from the last

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -76,7 +76,7 @@ function Home() {
                         const date = new Date(movie.released);
                         const year = date.getFullYear();
                         const poster = movie.poster;
-                        const last = Movies.length === index + 1 ? true : false;
+                        const last = Movies.length === index + 4 ? true : false;
                         if (last) {
                             return (
                                 <div
